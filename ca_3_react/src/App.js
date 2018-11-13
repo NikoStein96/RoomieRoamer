1b7d4be0b06652af6dc16174e54da2cfc2b2f8a9
Swoop86dk <cph-mr373@cphbusiness.dk>
2018-11-13 10:29:52 +0100

    api facade

--- ca_3_react/src/App.js
+++ ca_3_react/src/App.js
@@ -1,54 +1,63 @@
-import React from "react";
-import { HashRouter as Router, Route, Link } from "react-router-dom";
-
-const App = () => (
-  <Router>
-    <div>
-      <Header />
-
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/topics" component={Topics} />
-    </div>
-  </Router>
-);
-
-const Home = () => <h2>Home</h2>;
-const About = () => <h2>About</h2>;
-const Topic = ({ match }) => <h3>Requested Param: {match.params.id}</h3>;
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-
-    <ul>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.path}/:id`} component={Topic} />
-    <Route
-      exact
-      path={match.path}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
-);
-const Header = () => (
-  <ul>
-    <li>
-      <Link to="/">Home</Link>
-    </li>
-    <li>
-      <Link to="/about">About</Link>
-    </li>
-    <li>
-      <Link to="/topics">Topics</Link>
-    </li>
-  </ul>
-);
-
+import React, { Component } from "react"
+import facade from "./apiFacade";
+class LogIn extends Component {
+ constructor(props) {
+   super(props);
+   this.state = { username: "", password: "" }
+ }
+ login = (evt) => {
+   evt.preventDefault();
+   this.props.login(this.state.username, this.state.password);
+ }
+ onChange = (evt) => {
+   this.setState({[evt.target.id]: evt.target.value})
+ }
+ render() {
+   return (
+     <div>
+       <h2>Login</h2>
+       <form onSubmit={this.login} onChange={this.onChange} >
+         <input placeholder="User Name" id="username" />
+         <input placeholder="Password" id="password" />
+         <button>Login</button>
+       </form>
+     </div>
+   )
+ }
+}
+class LoggedIn extends Component {
+ constructor(props) {
+   super(props);
+   this.state= {dataFromServer: "Fetching!!"};
+ }
+ componentDidMount(){}
+ render() {
+   return (
+     <div>
+       <h2>Data Received from server</h2>
+       <h3>{this.state.dataFromServer}</h3>
+     </div>
+   )
+ }
+}
+class App extends Component {
+ constructor(props) {
+   super(props);
+   this.state = { loggedIn: false }
+ }
+ logout = () => {} //TODO
+ login = (user, pass) => {facade.login(user,pass)
+  .then(res =>this.setState({ loggedIn: true }));} 
+ render() {
+   return (
+     <div>
+       {!this.state.loggedIn ? (<LogIn login={this.login} />) :
+         ( <div>
+             <LoggedIn/>
+             <button onClick={this.logout}>Logout</button>
+           </div>)}
+     </div>
+   )
+ }
+}
 export default App;
