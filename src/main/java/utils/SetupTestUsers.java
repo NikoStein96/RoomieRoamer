e9c2cbb6f5aede2ca211caf39987092bfbbf9bdb
Tarllark <33085010+Tarllark@users.noreply.github.com>
2018-11-16 14:52:47 +0100

    Fixed persistence

--- src/main/java/utils/SetupTestUsers.java
+++ src/main/java/utils/SetupTestUsers.java
@@ -18,22 +18,22 @@ public class SetupTestUsers {
     // throw new UnsupportedOperationException("REMOVE THIS LINE, WHEN YOU HAVE READ WARNING");
     
        
-//    em.getTransaction().begin();
+    em.getTransaction().begin();
 //    Role userRole = new Role("user");
-//    Role adminRole = new Role("admin");
+    Role adminRole = new Role("admin");
 //    User user = new User("user", "test123");
 //    user.addRole(userRole);
-//    User admin = new User("admin", "test123");
-//    admin.addRole(adminRole);
+    User admin = new User("admin", "test123");
+    admin.addRole(adminRole);
 //    User both = new User("user_admin", "test123");
 //    both.addRole(userRole);
 //    both.addRole(adminRole);
 //    em.persist(userRole);
 //    em.persist(adminRole);
 //    em.persist(user);
-//    em.persist(admin);
+    em.persist(admin);
 //    em.persist(both);
-//    em.getTransaction().commit();
+    em.getTransaction().commit();
 ////    System.out.println("PW: " + user.getUserPass());
 //    System.out.println("Testing user with OK password: " + user.verifyPassword("test"));
    // User user = em.find(User.class, "user");
