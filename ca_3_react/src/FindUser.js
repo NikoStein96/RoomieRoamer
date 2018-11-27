2e635fdc76c4c7cb6cc9b3ea5b58a05edd87bbfc
Nikolai Stein <nikolai_stein@hotmail.com>
2018-11-27 10:02:57 +0100

    Update FindUser.js

--- ca_3_react/src/FindUser.js
+++ ca_3_react/src/FindUser.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 export default class FindUser extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = { value: "", json: [] };
   }
 
   handleChange = event => {
@@ -12,31 +12,41 @@ export default class FindUser extends Component {
 
   handleSubmit = event => {
     event.preventDefault();
-    var URL = "https://swapi.co/api/people/" + this.state.value;
+    var URL = "http://localhost:8084/RoomieRoamer/api/User/" + this.state.value;
     fetch(URL)
       .then(response => response.json())
       .then(json => {
-        console.log(json);
-        document.getElementById("info").innerHTML =
-          "Name: " + json.name + "  mass: " + json.mass;
+        this.setState({ json: json });
       });
   };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <p>{this.state.value}</p>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="number"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
 
-        <input
-          type="number"
-          value={this.state.value}
-          onChange={this.handleChange}
-        />
+          <input type="submit" value="Submit" />
 
-        <input type="submit" value="Submit" />
+          <p id="info" />
+        </form>
 
-        <p id="info" />
-      </form>
+        <p>
+          {this.state.json.id} <br />
+          {this.state.json.userName} <br />
+          {this.state.json.desc} <br />
+          {this.state.json.picRef} <br />
+          {this.state.json.roleList} <br />
+          {this.state.json.interests} <br />
+          {this.state.json.liked} <br />
+          {this.state.json.ignored} <br />
+          {this.state.json.matches}
+        </p>
+      </div>
     );
   }
 }
