b4cfc11a8b81bd2b8057fc2bd7e35cf50009d66d
fb05 <32671386+fb05@users.noreply.github.com>
2018-11-28 10:52:09 +0100

    Get description out for user

--- ca_3_react/src/UserPage.js
+++ ca_3_react/src/UserPage.js
@@ -4,7 +4,8 @@ import facade from "./apiFacade";
 export default class UserPage extends Component {
     constructor(props) {
       super(props);
-      this.state = { dataFromServer: ""}
+      this.state = { dataFromServer: "",}
+      this.state = { value: "" };
     }
   
   // fetch user and admin details from backend
@@ -12,6 +13,31 @@ export default class UserPage extends Component {
       facade.fetchData().then(res => this.setState({ dataFromServer: res }));
       facade.fetchDataAdmin().then(res => this.setState({ dataFromServer: res}));
     }
+
+    handleChangeUP = event => {
+      console.log(event.target.value);
+      this.setState({ value: event.target.value });
+    };
+
+    handleSubmitUP = event => {
+      event.preventDefault();
+      var URL = "http://localhost:8080/RoomieRoamer/api/User/"+this.state.value+"/desc";
+      fetch(URL)
+        .then(response => response.json())
+        .then(json => {
+          console.log(json);
+          if(json === undefined){
+            document.getElementById("desc1").innerHTML = "No user with that userID";
+          }
+          else{
+  
+          document.getElementById("desc1").innerHTML =
+          // eslint-disable-next-line
+          json;
+  
+          }
+        });
+    };
   
     render() {
       return (
@@ -23,9 +49,33 @@ export default class UserPage extends Component {
           <br>
           </br>
           <button onClick={facade.logout}>Logout</button>
-        </div>
+        
+        <br/>
+        <br/>
+        <br/>
+        
+        <form onSubmit={this.handleSubmitUP}>
+        
+
+        <input
+          type="number"
+          value={this.state.value}
+          onChange={this.handleChangeUP}
+        />
+
+        <input type="submit" value="Search" />
+        <h3>We will fetch user describtion with ID: {this.state.value}</h3>
+
+        
+        <p id="desc1" align="center">no data yet</p>
         
         
+        
+        
+        
+      </form>
+      </div>  
+      
       )
   
     }
